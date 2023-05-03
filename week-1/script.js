eeee7d689ff04c93d6ac2f642acadb1b2eacc348
Charlene Tolentino <tolentino.charlene@gmail.com>
2023-05-03 14:32:48 -0700

    assignment 1

---
+++ week-1/script.js
@@ -0,0 +1,11 @@
+let firstName = "Charlene"
+let lastName = "Tolentino";
+let yearsOfStudy = 1;
+let  goal = 'become a debecome a technical project manager';
+
+//("My name is " + firstName + " " + lastName + ". I have been at SCC for " + yearsOfStudy + " years and plan to " + goal + " after completing my courses.");
+
+
+let introduction = `My name is ${firstName} ${lastName}. I have been at Seattle Central College for ${yearsOfStudy} year and plan to ${goal} after completing my courses.`; 
+
+document.getElementById("introduction").innerHTML = introduction;
